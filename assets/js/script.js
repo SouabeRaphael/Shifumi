cd14785afdbb0bae31c901ab924e94b8c92b557c
ChauveauAdrien <adrienpro.chauveau@gmail.com>
2022-01-11 21:40:36 +0100

    fin du drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,63 @@
+// --------------------------------------------------------------------------------------------
+// ---------------------------Fonction Drag and Drop ------------------------------------------
+// --------------------------------------------------------------------------------------------
+function dragNdrop() {
+  item = $(".item");
+  item.on("dragstart", dragStart);
+  item.on("dragend", dragEnd);
 
-  // Drag and Drop
-function dragCard() {
-  const card = $(".img-card");
-  var choice;
-  card.mousedown(function(){
-	choice = $(this).html();
-  })
-  card.draggable();
-  $(".game-area").droppable({
-    drop: function (event, ui) {
-      $(this).css('background', 'red');
-	  console.log(choice);
-    //   $(this).css({ background: "green" });
-    },
-  });
+  // dragStart: lorsque qu'on "prend" la carte
+  function dragStart() {
+    $(this).addClass('active');
+    // console.log("start");
+  }
+
+  //  dragEnd: lorsque qu'on "relache" la carte
+  function dragEnd() {
+    $(this).removeClass('active');
+    // console.log("end");
+  }
+
+  // boucle qui nique ta grosse daronne
+  $('.first-card').each(function(){
+    $(this).on('dragover', dragOver);
+    // $(this).on('dragenter', dragEnter);
+    // $(this).on('dragleave', dragLeave);
+    $(this).on('drop', drop);
+})
+  function dragOver(e) {
+    e.preventDefault();
+  }
+
+  // function dragEnter(e) {
+  // }
+
+  // function dragLeave(){
+  // }
+
+  // au moment du relachement 
+  function drop(){
+    let item = $('.active')
+    let itemValue = item.attr('value')
+    // console.log('drop');
+    $(this).append(item)
+    $(this).attr('value', itemValue)
+    console.log($(this).attr('value'));
+  }
 }
 
+// function play() {
+//   let card = $(".item");
+//   console.log(card);
+//   card.click(function () {
+//     let value = $(this).attr("value");
+//     console.log(value);
+//   });
+// }
 
 // on execute les fonctions quand le DOM est chargé
 
 $(function () {
-  dragCard();
+  dragNdrop();
+  // play()
 });
