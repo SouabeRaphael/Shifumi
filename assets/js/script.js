0cf6fe3fb071fbed7ac5ad0b0516e76cdf8f37a6
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-01-11 12:12:02 +0100

    smdkifnjsd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
-$( function() {
-	// Drag and Drop
-	$( function() {
-		$( "#draggable" ).draggable();
-		$( "#droppable" ).droppable({
-		  	drop: function( event, ui ) {
-				$( this )
-				.addClass( "ui-state-highlight" )
-				.find( "p" )
-				.text() = $( "#draggable" ).text();
-		  	}
-		});
+  // Drag and Drop
+function draggAndDrop() {
+	// Attribution de draggable
+	$( "#feuille" ).draggable();
+	$( "#pierre" ).draggable();
+	$( "#ciseaux" ).draggable();
+	// Attribution de droppable
+	$( ".droppable" ).droppable({
+		drop: function( event, ui ) {
+			// Change le texte du block
+			$( this ).find( "p" ).text(choice);
+			$( this ).css({'background':'green'});
+		}
 	});
-});
+};
+
+$(function(){
+	draggAndDrop();
+})
