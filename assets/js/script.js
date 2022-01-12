d9b995a9a6f06705bfadb71ff251d7e58d98d1ec
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-01-12 08:03:13 +0100

    fvdqf

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,49 @@
-let item = $('div');
-console.log(item);
 
-document.querySelector('.card-rock').addEventListener('click', function(){
-  console.log("salut");
-})
+function dragAndDrop(){
+    item = $('.item');
+    item.on("dragstart", function(){
+        console.log('dragstart');
+        $(this).addClass('active');
+      });
+    item.on("dragend", function(){
+        // console.log('dragend');
+        $(this).removeClass('active');
+      });
+    $('.first-card').each(function(){
+        $(this).on('dragover', dragOver);
+        // $(this).on('dragenter', dragEnter);
+        // $(this).on('dragleave', dragLeave);
+        $(this).on('drop', drop);
+    })
+    function dragOver(e) {
+        // console.log("dragover")
+        e.preventDefault()
+    }
+    
+    // function dragEnter(e) {
+    //     console.log("dragenter")
+    //     e.preventDefault();
+    // }
+    
+    // function dragLeave() {
+    //     console.log("dragleave")
+    //     // this.className = 'case';
+    // }
+    
+    function drop() {
+        // console.log("drop");
+        let item = $('.active');
+        $(this).append(item);
+        let value = item.attr("value");
+        $(this).attr('value', value);
+        console.log($(this).attr("value"));
+    }
+}
+  // on execute les fonctions quand le DOM est chargé
+  
+
+  $(function () {
+    dragAndDrop();
+  });
 
 
